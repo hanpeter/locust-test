7960439c637e7d17a595beab7814bc86d5724104
Peter Han <phan@salesforce.com>
2019-05-05 20:09:29 -0700

    Dynamically generate Locust file based on a YAML file

--- locust_runner/runner.py
+++ locust_runner/runner.py
@@ -1,41 +1,46 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
 import os
-import json
-from locust import HttpLocust, TaskSet, task
+import yaml
+from locust import HttpLocust, TaskSet
 
 
-class Task(TaskSet):
-    @task
-    def get(self):
-        url = os.environ.get('LOCUST_GET_URL', None)
-        params = json.loads(os.environ.get('LOCUST_GET_PARAMS', '{}'))
-        auth = tuple(json.loads(os.environ.get('LOCUST_GET_AUTH', '[]')))
+class Config(dict):
+    def __init__(self, config_file):
+        # Call super class and be a good Python class
+        super(Config, self).__init__()
 
-        if url is not None:
-            response = self.client.get(
-                url=url,
-                params=params,
-                auth=auth,
-            )
-            print(response.status_code)
-
-    @task
-    def post(self):
-        url = os.environ.get('LOCUST_POST_URL', None)
-        params = json.loads(os.environ.get('LOCUST_POST_PARAMS', '{}'))
-        auth = tuple(json.loads(os.environ.get('LOCUST_POST_AUTH', '[]')))
-
-        if url is not None:
-            response = self.client.post(
-                url=url,
-                json=json,
-                auth=auth,
-            )
-            print(response.status_code)
+        with open(config_file) as f:
+            self.update(yaml.safe_load(f))
 
 
-class Locust(HttpLocust):
-    task_set = Task
-    min_wait = os.environ.get('LOCUST_MIN_WAIT', 1000)
-    max_wait = os.environ.get('LOCUST_MAX_WAIT', 1000)
+def task_builder(url, config):
+    def task(locust):
+        locust.client.request(
+            method=config.get('method', 'GET'),
+            url=url,
+            headers=config.get('headers', {}),
+            params=config.get('params', {}),
+            auth=config.get('auth', None)
+        )
+
+    return task
+
+
+config = Config(config_file=os.path.join(os.path.dirname(__file__), '..', 'config.yaml'))
+domain = next(key for key in config.keys() if key != 'min_wait' and key != 'max_wait' and key != 'ssl')
+
+task_list = []
+for url, config in config[domain].items():
+    task_list.append(task_builder(url, config))
+
+Tasks = type('Tasks', (TaskSet,), {'tasks': task_list})
+
+attributes = {
+    'task_set': Tasks,
+    'min_wait': config.get('min_wait', 1000),
+    'max_wait': config.get('max_wait', 1000),
+    'host': ('https://' if config.get('ssl', True) else 'http://') + domain
+}
+
+Locust = type('Locust', (HttpLocust,), attributes)
